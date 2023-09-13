b4fe48adafc932a172473109c6c5827b91b272fd
202120423 <202120423@TPCP04LAB1015>
2023-09-13 18:44:45 -0300

    Adicionar arquivos de projeto.

---
+++ trabalho_avd1/Conta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabalho_avd1
+{
+    internal class Conta
+    {
+        private decimal _saldo;
+
+        public int Numero { get; set; }
+    }
+}
---
+++ trabalho_avd1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using trabalho_avd1;
+
+namespace ControleContas
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Conta conta1 = new Conta();
+            conta1.Numero = 123456;
+            Console.WriteLine(conta1.Numero);
+        }
+    }
+}
